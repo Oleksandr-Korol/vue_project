7eb01fd1c9bdc3929a5bc348abc43fb06f31420f
Oleksa Korol <oleksandrwinner@gmail.com>
2020-08-28 11:39:28 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>About page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus optio minima vitae hic a ullam distinctio recusandae perspiciatis libero alias, unde iure debitis eveniet rem magni est quasi dignissimos odio.
+        </p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit atque incidunt aut pariatur nostrum id itaque nihil debitis rerum harum nulla, animi enim et optio illo, at dolore saepe dolorem.
+        </p>
+    </section>
+</template>
+<script>
+export default {
+    middleware: ['auth']
+}
+</script>
